98d408fdaa0476f4669d93860bee8ab314ecd35f
gileoo <m.ritter@ahm.co.at>
2017-07-18 12:01:04 +0200

    adjust comment

--- ImguiWindowsAboutIGS.hpp
+++ ImguiWindowsAboutIGS.hpp
@@ -8,15 +8,7 @@ void drawIGSLogo(
 
 	
 /** Show an info about window with animated IGS logo. 
-	Usage example:
-	if( about_window_visible &&  
-        aboutWindow( 
-			ICON_FA_INFO_CIRCLE " About",
-			"My fancy tools\n"
-			"Abla bli blub ....\n",
-			ImVec2( 400, 155 ),
-			time ) )
-			about_window_visible = false;
+* \param time in secs.
 */
 bool aboutWindow(
     const char* title,
