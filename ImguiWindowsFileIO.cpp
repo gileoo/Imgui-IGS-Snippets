d228a62a0d911539ffe76e42be6fba0bc6fde64c
Nikolaus Rauch <nikolaus.rauch.nr@gmail.com>
2017-07-20 12:18:59 +0200

    linux support for file dialog (dirent.h and stat.h of gnu)

--- ImguiWindowsFileIO.cpp
+++ ImguiWindowsFileIO.cpp
@@ -1,51 +1,53 @@
-#include "ImguiWindowsFileIO.hpp"
+#include "ImguiWindowsFileIO.h"
 
 #include <algorithm>
 #include <cstdlib>
 #include <sstream>
 
 #if defined(WIN32)
-    #include <windows.h>
-    #include <direct.h>
-    #include <tchar.h>  
-    #define GetCurrentDir _getcwd
+#include <windows.h>
+#include <direct.h>
+#include <tchar.h>
+#define GetCurrentDir _getcwd
 #else
-    #include <unistd.h>
-    #define GetCurrentDir getcwd
- #endif
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#define GetCurrentDir getcwd
+#endif
 
 #define IM_ARRAYSIZE(_ARR)  ((int)(sizeof(_ARR)/sizeof(*_ARR)))
 
 #if defined(ICON_FA_CARET_DOWN)
-    #define CARET_DOWN ICON_FA_CARET_DOWN
+#define CARET_DOWN ICON_FA_CARET_DOWN
 #else
-    #define CARET_DOWN "v"
+#define CARET_DOWN "v"
 #endif
 
 using namespace std;
 using namespace ImGui;
- 
+
 #if defined(WIN32)
 vector<string> getWindowsDrives()
 {
     vector<string> result;
     TCHAR g_szDrvMsg[] = _T("A:");
 
-    ULONG uDriveMask = _getdrives();  
-  
-    if (uDriveMask == 0)  
-    {}  
-    else  
-    {  
-        while (uDriveMask) 
-        {  
-            if (uDriveMask & 1)  
+    ULONG uDriveMask = _getdrives();
+
+    if (uDriveMask == 0)
+    {}
+    else
+    {
+        while (uDriveMask)
+        {
+            if (uDriveMask & 1)
                 result.push_back( g_szDrvMsg );
-  
-            ++g_szDrvMsg[0];  
-            uDriveMask >>= 1;  
-        }  
-   }  
+
+            ++g_szDrvMsg[0];
+            uDriveMask >>= 1;
+        }
+    }
 
     return result;
 }
@@ -53,11 +55,11 @@ vector<string> getWindowsDrives()
 
 vector<string> stringSplit(const string &s, char delim)
 {
-vector<string> elems;
-string item; 
+    vector<string> elems;
+    string item;
 
-// use stdlib to tokenize the string
-stringstream ss(s);
+    // use stdlib to tokenize the string
+    stringstream ss(s);
     while (getline(ss, item, delim))
         if(!item.empty())
         {
@@ -65,6 +67,11 @@ stringstream ss(s);
         }
 
     return elems;
+}
+
+bool compareNoCase(const string& first, const string& second)
+{
+    return strcasecmp( first.c_str(), second.c_str() ) <= 0;
 }
 
 string MiniPath::filePath() const
@@ -114,9 +121,9 @@ MiniPath::MiniPath( const string& some_path )
 string MiniPath::getDelim() const
 {
     if (path.find ("/") != string::npos) // linux style
-            return "/";
+        return "/";
     else if (path.find ("\\") != string::npos) // windows style
-            return "\\";
+        return "\\";
     else
     {
         return MiniPath::getSystemDelim();
@@ -125,13 +132,13 @@ string MiniPath::getDelim() const
 
 std::string MiniPath::getSystemDelim()
 {
-        string current = MiniPath::getCurrentDir();
-        if (current.find ("/") != string::npos) // linux style
-            return "/";
-        else if (current.find ("\\") != string::npos) // windows style
-            return "\\";
-        else
-            return "/";
+    string current = MiniPath::getCurrentDir();
+    if (current.find ("/") != string::npos) // linux style
+        return "/";
+    else if (current.find ("\\") != string::npos) // windows style
+        return "\\";
+    else
+        return "/";
 }
 
 string MiniPath::prefix() const
@@ -174,7 +181,7 @@ string MiniPath::getPath() const
 
 vector<string> MiniPath::getPathTokens() const
 {
-    if( !getDelim().empty() ) 
+    if( !getDelim().empty() )
         return stringSplit( path + getDelim(), getDelim()[0] );
 
     vector<string> tmp;
@@ -222,6 +229,7 @@ bool MiniPath::isAbsoluteFilePath( const string& s )
 
 std::list<string> MiniPath::listDirectories( const string& s )
 {
+#if defined(WIN32)
     list<string> directories;
 
     struct _finddata_t c_file;
@@ -233,19 +241,44 @@ std::list<string> MiniPath::listDirectories( const string& s )
     {}
     else
     {
-        do 
-        {  
-            char buffer[256];  
+        do
+        {
+            char buffer[256];
             if( ( c_file.attrib & _A_SUBDIR ) )
                 directories.push_back( string( c_file.name ) );
-        } while( _findnext( hFile, &c_file ) == 0 );        
+        } while( _findnext( hFile, &c_file ) == 0 );
     }
 
     return directories;
+#else
+
+    list<string> directories;
+
+    string path = s;
+
+    DIR* directory = opendir(path.c_str());
+    if(directory != NULL)
+    {
+
+        struct dirent* entry;
+        while( (entry = readdir(directory)) != NULL )
+        {
+            if(entry->d_type == DT_DIR)
+                directories.push_back(string(entry->d_name));
+        }
+
+        closedir(directory);
+    }
+
+    directories.sort(compareNoCase);
+
+    return directories;
+#endif
 }
 
 std::list<string> MiniPath::listFiles( const string& s, string filter )
 {
+#if defined(WIN32)
     list<string> files;
 
     struct _finddata_t c_file;
@@ -257,26 +290,67 @@ std::list<string> MiniPath::listFiles( const string& s, string filter )
     {}
     else
     {
-        do 
-        {  
-            char buffer[256];  
+        do
+        {
+            char buffer[256];
             if( !( c_file.attrib & _A_SUBDIR ) )
                 files.push_back( string( c_file.name ) );
-        } while( _findnext( hFile, &c_file ) == 0 );        
+        } while( _findnext( hFile, &c_file ) == 0 );
     }
 
     return files;
+#else
+
+    list<string> directories;
+
+    string path = s;
+
+    DIR* directory = opendir(path.c_str());
+    if(directory != NULL)
+    {
+
+        struct dirent* entry;
+        while( (entry = readdir(directory)) != NULL )
+        {
+            if(entry->d_type == DT_REG)
+            {
+                vector<string> splits = stringSplit(string(entry->d_name),'.');
+                vector<string> filtersplit = stringSplit(filter, '.');
+
+                if(filter.empty() || filtersplit.back().compare("*") == 0 ||  splits.back().compare(filtersplit.back()) == 0)
+                    directories.push_back(string(entry->d_name));
+            }
+        }
+
+        closedir(directory);
+    }
+
+    directories.sort(compareNoCase);
+
+    return directories;
+#endif
+
 }
 
 bool MiniPath::pathExists( const std::string& s )
 {
+#if defined(WIN32)
     struct _finddata_t c_file;
-    intptr_t hFile; 
+    intptr_t hFile;
 
     if( (hFile = _findfirst( s.c_str(), &c_file )) != -1L )
         return true;
 
     return false;
+#else
+
+    struct stat info;
+
+    if(  stat(s.c_str(), &info) == 0 )
+        return true;
+
+    return false;
+#endif
 }
 
 vector<const char*> toCStringVec( const vector<string>& vs )
@@ -296,12 +370,12 @@ vector<const char*> toCStringVec( const list<string>& vs )
 }
 
 bool fileIOWindow(
-    string& file_path,
-    std::vector<string>& recently_used_files,
-    const string& button_text,
-    std::vector<std::string> file_filter,
-    bool ensure_file_exists,
-    ImVec2 size )
+        string& file_path,
+        std::vector<string>& recently_used_files,
+        const string& button_text,
+        std::vector<std::string> file_filter,
+        bool ensure_file_exists,
+        ImVec2 size )
 {
     bool close_it = false;
 
@@ -335,18 +409,18 @@ bool fileIOWindow(
     }
     
     list<string> local_files = MiniPath::listFiles( current_mini_path.getPath(), string( extension_cstrings[file_type_selected] ) );
-    vector<const char*> file_list = toCStringVec( local_files ); 
+    vector<const char*> file_list = toCStringVec( local_files );
 
-    if ( directory_browsing ) 
+    if ( directory_browsing )
         size.y += std::min( size_t( 8 ), std::max( dir_list.size(), file_list.size() ) ) *  GetFontSize();
 
     SetNextWindowSize( size );
     Begin( "FileIO" );
 
     Text("Directory: "); SameLine();
-    PushItemWidth( GetWindowWidth() - 145 ); 
+    PushItemWidth( GetWindowWidth() - 145 );
 
-    strcpy( c_current_folder, current_folder.c_str() ); 
+    strcpy( c_current_folder, current_folder.c_str() );
     if( InputText( " ", c_current_folder, IM_ARRAYSIZE( c_current_folder ) ) )
     {
         string tmp = c_current_folder;
@@ -365,7 +439,7 @@ bool fileIOWindow(
         if( ListBox( "", &recent_selected, recent.data(), recent.size() ) )
         {
             current_mini_path.fromString( string( recent[recent_selected] ), sys_delim[0] );
-            if( !current_mini_path.getName().empty() ) 
+            if( !current_mini_path.getName().empty() )
                 strcpy( current_file, current_mini_path.getName().c_str() );
             current_folder = current_mini_path.getPath();
             CloseCurrentPopup();
@@ -379,7 +453,12 @@ bool fileIOWindow(
     {
         if( Button( split_directories[i].c_str() ) )
         {
+#if defined(WIN32)
             string chosen_dir;
+#else
+            string chosen_dir = "/";
+#endif
+
             for( int j = 0; j <= i; ++j)
             {
                 chosen_dir += split_directories[j];
@@ -387,7 +466,7 @@ bool fileIOWindow(
                     chosen_dir += sys_delim;
             }
             current_folder = chosen_dir;
-        } 
+        }
         if( i != split_directories.size()-1 )
             SameLine();
     }
@@ -402,7 +481,7 @@ bool fileIOWindow(
         // select list item dependent on folder path
         if( drive_list[drive_selected][0] != c_current_folder[0]  )
             for( int i = 0; i < drive_list.size(); ++i )
-               if( c_current_folder[0] == drive_list[i][0] )
+                if( c_current_folder[0] == drive_list[i][0] )
                     drive_selected = i;
 
         Text( "  " ); SameLine();
@@ -426,6 +505,9 @@ bool fileIOWindow(
             {
                 vector<string> split_directories = current_mini_path.getPathTokens();
                 current_folder.clear();
+#ifndef WIN32
+                current_folder += "/";
+#endif
                 for( int i = 0; i < split_directories.size() - 1; ++i )
                 {
                     current_folder += split_directories[i];
@@ -445,14 +527,14 @@ bool fileIOWindow(
         }
     }
     else
-         directory_browsing = false;
+        directory_browsing = false;
 
     Text(" ");
 
     Text("File Name: "); SameLine();
     InputText( "  ", current_file, IM_ARRAYSIZE( current_file ) );
 
-    Text( "File Type: " ); SameLine(); Text( extension_cstrings[file_type_selected] ); SameLine();    
+    Text( "File Type: " ); SameLine(); Text( extension_cstrings[file_type_selected] ); SameLine();
     if (Button( CARET_DOWN ) )
         ImGui::OpenPopup("FileType");
 
@@ -474,8 +556,8 @@ bool fileIOWindow(
     if( Button( button_text.c_str() ) )
     {
         vector<string> ext_filter_v = stringSplit( extension_cstrings[file_type_selected], '.');
-        if( ext_filter_v.size() == 2 && 
-            ext_filter_v[1] != "*" )
+        if( ext_filter_v.size() == 2 &&
+                ext_filter_v[1] != "*" )
         {
             string ext_filter = ext_filter_v[1];
             if( current_mini_path.extension() == ext_filter )
@@ -484,9 +566,9 @@ bool fileIOWindow(
         else
             file_path = current_mini_path.filePath();
 
-        if( ensure_file_exists ) 
+        if( ensure_file_exists )
         {
-            if( MiniPath::pathExists( file_path ) ) 
+            if( MiniPath::pathExists( file_path ) )
                 close_it = true;
         }
         else
