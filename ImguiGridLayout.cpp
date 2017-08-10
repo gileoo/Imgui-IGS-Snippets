8a73c030d86c4aa91cd7b47aa0c061edaaecafe6
gileoo <m.ritter@ahm.co.at>
2017-08-10 15:33:09 +0200

    correct namespaces

--- ImguiGridLayout.cpp
+++ ImguiGridLayout.cpp
@@ -1,5 +1,7 @@
 #include "ImguiGridLayout.hpp" 
 
+using namespace ImGui;
+
 GridLayout::GridLayout( 
     const ImVec2& screen_size,
     ImVec2 grid_division,
@@ -10,10 +12,10 @@ GridLayout::GridLayout(
 {}
 
 void GridLayout::placeNextWindow( 
-        const ImVec2& corner1, 
-        const ImVec2& corner2, 
-        const char* mode ,
-        int border ) const
+    const ImVec2& corner1, 
+    const ImVec2& corner2, 
+    const char* mode ,
+    int border ) const
 {
     if( !active )
         return;
@@ -31,11 +33,12 @@ void GridLayout::placeNextWindow(
     c2.x = (mode[2] == 'l') ? corner2.x * dx : screen_size_.x - corner2.x * dx;
     c2.y = (mode[3] == 't') ? corner2.y * dy : screen_size_.y - menu_height_ - corner2.y * dy;   
 
-    ImGui::SetNextWindowPos ( ImVec2( c1.x + border, c1.y + border + menu_height_ ) );
-    ImGui::SetNextWindowSize( ImVec2( c2.x - c1.x - border, c2.y - c1.y - border) ); 
+    SetNextWindowPos ( ImVec2( c1.x + border, c1.y + border + menu_height_ ) );
+    SetNextWindowSize( ImVec2( c2.x - c1.x - border, c2.y - c1.y - border) ); 
 }
 
-void GridLayout::resize( const ImVec2& screen_size )
+void GridLayout::resize( 
+	const ImVec2& screen_size )
 {
     screen_size_.x = screen_size.x;
     screen_size_.y = screen_size.y;
--- ImguiGridLayout.hpp
+++ ImguiGridLayout.hpp
@@ -1,12 +1,11 @@
 #pragma once 
 
 #include <imgui.h>
-#include "imgui_impl_gf.h"
 
 class GridLayout
 {
-    ImVec2 screen_size_;
-    ImVec2 grid_division_;
+    ImGui::ImVec2 screen_size_;
+    ImGui::ImVec2 grid_division_;
 
     int menu_height_ { 20 };
 
@@ -14,15 +13,15 @@ public:
     bool active { true };
 
     GridLayout( 
-        const ImVec2& screen_size,
-        ImVec2 grid_division = ImVec2( 10, 10 ),
+        const ImGui::ImVec2& screen_size,
+        ImGui::ImVec2 grid_division = ImVec2( 10, 10 ),
         int menu_height = 20 );
 
     void placeNextWindow( 
-            const ImVec2& corner1, 
-            const ImVec2& corner2, 
-            const char* mode ,
-            int border = 1 ) const;
+        const ImGui::ImVec2& corner1, 
+        const ImGui::ImVec2& corner2, 
+        const char* mode ,
+        int border = 1 ) const;
     
-    void resize( const ImVec2& screen_size ); 
+    void resize( const ImGui::ImVec2& screen_size ); 
 };
