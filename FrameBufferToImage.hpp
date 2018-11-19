6eec3924980365d79d20f9cf01b1f8f23a2489aa
marcel <marcel.ritter@uibk.ac.at>
2018-11-19 12:40:14 +0100

    add link sources

--- FrameBufferToImage.hpp
+++ FrameBufferToImage.hpp
@@ -2,12 +2,14 @@
 
 #include "GL/glew.h"
 
+//http://www.libpng.org/pub/png/libpng.html
 #include "png.h"
 
 void writePngFile(const char *filename, int width, int height, GLubyte* pixels );
 void saveFramePNG( std::string file_name );
 
 
+//https://github.com/nothings/stb
 #include "stb.h"
 
 void saveFrameJpegSTB( std::string file_name );
